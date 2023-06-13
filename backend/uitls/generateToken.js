660f7594e343e6c3042efb2bd3225c2e13d2582e
Vishal Chauhan <vishalchauhan@Vishals-MacBook-Air.local>
2023-06-13 09:48:53 +0530

    cors cookies

--- backend/uitls/generateToken.js
+++ backend/uitls/generateToken.js
@@ -7,7 +7,7 @@ const generateToken = (res, userId) => {
   res.cookie("jwtToken", token, {
     httpOnly: true,
     secure: process.env.NODE_ENV != "development",
-    sameSite: "strict",
+    sameSite: "none",
     maxAge: 30 * 24 * 60 * 60 * 1000,
   });
 };
