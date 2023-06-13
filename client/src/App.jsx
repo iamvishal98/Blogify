575fe68cfaa9e9c7644b68d52a0570a78951dd0c
Vishal Chauhan <vishalchauhan@Vishals-MacBook-Air.local>
2023-06-13 10:06:23 +0530

    added protected route component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,8 +10,11 @@ import "react-toastify/dist/ReactToastify.css";
 import CreatePost from "./pages/CreatePost";
 import PostPage from "./pages/PostPage";
 import EditPost from "./pages/EditPost";
+import { useSelector } from "react-redux";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 const App = () => {
+  const { user } = useSelector((state) => state.auth);
   return (
     <BrowserRouter>
       <Routes>
@@ -19,9 +22,23 @@ const App = () => {
           <Route index element={<IndexPage />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Signup />} />
-          <Route path="/create" element={<CreatePost />} />
+          <Route
+            path="/create"
+            element={
+              <ProtectedRoute user={user}>
+                <CreatePost />
+              </ProtectedRoute>
+            }
+          />
           <Route path="/post/:id" element={<PostPage />} />
-          <Route path="/post/edit/:id" element={<EditPost />} />
+          <Route
+            path="/post/edit/:id"
+            element={
+              <ProtectedRoute user={user}>
+                <EditPost />
+              </ProtectedRoute>
+            }
+          />
         </Route>
       </Routes>
       <ToastContainer />
